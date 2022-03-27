d5fac3daf1750257982da80e4c03efcbe6e9afdf
Laita Zidan <2131710100@polinema.ac.id>
2022-03-27 10:15:46 +0700

    minggu5

--- minggu5/praktik_php/konstanta.php
+++ minggu5/praktik_php/konstanta.php
@@ -7,7 +7,7 @@
             $var = "ini adalah contoh variabel ";
             echo $var;
             echo "<hr><br>";
-            define("konstanta", "ini adalah contoh konstanta");
+            define("konstanta", "ini adalah contoh konstanta", true);
             echo konstanta;
             echo "<br>";
             echo KONSTANTA;
