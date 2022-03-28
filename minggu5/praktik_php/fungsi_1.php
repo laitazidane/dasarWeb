09ccc5d20005b88448e086be187ec284be2b434e
Laita Zidan <2131710100@polinema.ac.id>
2022-03-28 08:04:06 +0700

    minggu5

---
+++ minggu5/praktik_php/fungsi_1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>    
+    </head>
+    <body>
+        <h2>Fungsi</h2>
+        <?php 
+            function writeMSG(){
+                echo "Hello World!";
+            }
+
+            writeMSG();
+         ?>
+    </body>
+</html>
