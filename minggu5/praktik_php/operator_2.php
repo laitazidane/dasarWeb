5c5d39c300cc28cb41044a2f5f5ca15e4be2409e
Laita Zidan <2131710100@polinema.ac.id>
2022-03-28 10:04:10 +0700

    minggu5

--- minggu5/praktik_php/operator_2.php
+++ minggu5/praktik_php/operator_2.php
@@ -8,9 +8,8 @@
             $b = 14;
             echo "$a < $b adalah "; var_dump($a < $b);
             echo "<br> $a > $b adalah "; var_dump($a > $b);
-            echo "<br> hasil perkalian $a dan $b adalah "; var_dump($a == $b);
-            echo "<br> hasil pembagian $a dan $b adalah "; var_dump($a != $b);
-            echo "<br> hasil sisa pembagian $a dan $b adalah "; var_dump($a < $b);
+            echo "<br>  $a == $b adalah "; var_dump($a == $b);
+            echo "<br> $a != $b adalah "; var_dump($a != $b);
          ?>
     </body>
 </html>
