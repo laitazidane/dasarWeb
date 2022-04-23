2b148e6671f9b1ac451f9c75708bdf7534cda061
Laita Zidan <2131710100@polinema.ac.id>
2022-04-23 20:30:54 +0700

    minggu9

---
+++ minggu9/praktik_php/hapusCookies.php
@@ -0,0 +1,13 @@
+<?php 
+    setcookie("mahasiswa", "dwd", time()-600);
+    setcookie("kelas","dwwww", time()-3600);
+ ?>
+ <html>
+     <body>
+         <?php 
+            echo "Cookies mahasiswa telah dihapus <br>";
+            echo "Cookies kelas telah dihapus"
+
+          ?>
+     </body>
+ </html>
